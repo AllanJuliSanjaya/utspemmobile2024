448b938a2e40dfa782d2141e629369cec52b1f0b
22091397072_Rachmadan Nauval <144086273+Rachmadan210@users.noreply.github.com>
2024-04-08 12:50:15 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
-import 'halaman_quiz.dart';
+import 'halaman_quiz.dart'; // Mengimpor file halaman_quiz.dart yang berisi widget HalamanQuiz
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // Menjalankan aplikasi Flutter dengan widget MyApp sebagai root widget
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}); // Deklarasi constructor MyApp
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Quiz App'),
-            ),
-            body: HalamanQuiz(),
-            ),
-        );
-    }
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Quiz App'), // Judul aplikasi di app bar
+        ),
+        body: HalamanQuiz(), // Menampilkan HalamanQuiz sebagai isi body aplikasi
+      ),
+    );
+  }
 }
