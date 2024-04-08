aaa41b99a775e3e79482d1b5b50826e1071ab11a
22091397072_Rachmadan Nauval <144086273+Rachmadan210@users.noreply.github.com>
2024-04-08 13:08:33 +0700

    Update question.dart

--- lib/question.dart
+++ lib/question.dart
@@ -1,8 +1,10 @@
+// Deklarasi kelas Pertanyaan untuk merepresentasikan pertanyaan dalam quiz
 class Pertanyaan {
-  String pertanyaan;
-  List<String> jawaban;
-  String kunciJawaban;
-  String gambar;
+  String pertanyaan; // Variabel untuk menyimpan teks pertanyaan
+  List<String> jawaban; // Variabel untuk menyimpan opsi jawaban dalam bentuk list
+  String kunciJawaban; // Variabel untuk menyimpan kunci jawaban
+  String gambar; // Variabel untuk menyimpan lokasi gambar terkait pertanyaan
 
+  // Konstruktor dengan parameter untuk menginisialisasi nilai variabel saat objek Pertanyaan dibuat
   Pertanyaan(this.pertanyaan, this.jawaban, this.kunciJawaban, this.gambar);
 }
